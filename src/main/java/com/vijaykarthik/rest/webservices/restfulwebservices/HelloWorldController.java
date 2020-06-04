68c19f5eccef7827c689e8992cdf123b71311307
Vijaykarthik <vijay.karthik@bpmlinks.com>
2020-06-04 15:51:40 +0530

    elevanth commit

--- src/main/java/com/vijaykarthik/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/vijaykarthik/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -17,14 +17,14 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World From Contineous Deployment Success Jawahar From Madurai";
+		return "Hello World From Contineous Deployment Success Navajith From Atlanta..";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		// throw new RuntimeException("Some Error has Happened! Contact Support at
 		// ***-***");
-		return new HelloWorldBean("Hello World Vijaykarthik Through Contineous Deployment...Jawahar ");
+		return new HelloWorldBean("Hello World Vijaykarthik Through Contineous Deployment...Navajith... ");
 	}
 
 	@GetMapping(path = "/hello-world/path-variable/{name}")
